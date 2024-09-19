5a7a1f5cbb37e3d08666d2be1b5fdd91bc4acd23
codewithme1122 <codewithme1212@gmail.com>
2024-09-19 20:57:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 class Person:
- name = "muddadsir"
- carear = "student"
- work = "python coding"
- 
+  name = "ALI"
+  occupation = "Software Developer"
+  networth = 10
+  def info(self):
+    print(f"{self.name} is a {self.occupation}")
+
+
+a = Person()
+b = Person()
+c = Person()
+
+a.name = "maqsooda"
+a.occupation = "principle of unique"
+
+b.name = "kashan"
+b.occupation = "a friend"
+
+# print(a.name, a.occupation)
+a.info()
+b.info()
+c.info()
