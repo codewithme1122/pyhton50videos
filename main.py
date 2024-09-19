89126358683c587b327c594c321af12389cdd70c
codewithme1122 <codewithme1212@gmail.com>
2024-09-19 20:55:09 +0500

    Add files via upload
    i done

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-#lambda funtion ko hum tub use krty hain jub hum code ko single line main likhty hain
-single = lambda x:x*2
-print(single(5))
-
-B = lambda x,y: x+y
-print(B(4,3))
-
-C = lambda a,b,c:(a+b+c)/3
-print(C(4,44,3))
-
+class Person:
+ name = "muddadsir"
+ carear = "student"
+ work = "python coding"
+ 
